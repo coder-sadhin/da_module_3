b3ecaa6d1b379e8d14722d64aec2ccb00c24166c
akkas_dev <codersadhin@gmail.com>
2024-06-12 01:48:57 +0800

    range base sum

---
+++ range_sum _quer.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,q;cin>>n>>q;
+    vector<int> lst(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin>>lst[i];
+    }
+    vector<int> prefix(n);
+    prefix[0]=lst[0];
+    for (int i = 1; i < n; i++)
+    {
+        prefix[i] = lst[i] + prefix[i-1];
+    }
+    
+    while (q--)
+    {
+       long long l,r;
+        cin>>l>>r;
+        l--;
+        r--;
+        long long sum;
+        if(l==0) sum=prefix[r];
+        else sum=prefix[r]-prefix[l-1];
+        cout<<sum<<endl;
+    }
+    return 0;
+}
